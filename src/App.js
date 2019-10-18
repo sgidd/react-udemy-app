f4467a6c212f1399325b9cfb76280c97f6b26d57
Sunil <sunilgidd051@gmail.com>
2019-10-18 09:47:54 +0530

    section -5 styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'; // Anything with Caps Start as export is Default
+import Radium, {StyleRoot} from 'radium';
 
 
 class App extends Component {
@@ -14,19 +15,6 @@ class App extends Component {
     otherStateProp: "other state property",
     showPersons: false
   }
-
-  // siwtchNameHandler = (newName) =>{
-  //   //console.log('switch was clicked !');
-  //   //Dont Do this :  this.state.persons[0].name ='Sunil Gidd';
-  //   this.setState({
-  //     persons : [
-  //       {name: newName , age: 27},
-  //       {name: "Manoj Dhamone" , age: 28},
-  //       {name: "pj" , age: 29}
-  //     ]
-  //   });
-
-  // };
 
   nameChangedHandler = (event) => {
     this.setState({
@@ -56,43 +44,28 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    // this.setState({persons: this.state.persons.splice(personIndex ,1)}); can not mdify the state directly so take
-    //in reference property and replace in setState
-
-    //const newPersons = this.state.persons;
-    //objects and arrays in javascript are reference types so we are pointing to the original persons array
-    //newPersons.splice(personIndex,1);// so here already mutating the original data which is not a correct way 
-    // so either copy array using SLICE (not splice) or create new array using spread operator as below
-
-    //const newPersons = this.state.persons.slice(); // plice with nothing copies the full array and returns new one
-      //or
-    const newPersons = [...this.state.persons]; //
+    const newPersons = [...this.state.persons]; 
     newPersons.splice(personIndex,1);
     this.setState({persons:newPersons});
   }
 
   render() {
     const style ={
-      backgroundColor : 'lightgrey',
-      color: '#10069f',
+      backgroundColor : 'green',
+      color: 'white',
       border:'1px solid tomato',
       padding: '8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover': {
+        backgroundColor :'lightgreen',
+        color:'black'
+      }
     }
 
     let persons = null;
 
     if(this.state.showPersons) {
       persons = (
-      // <div>
-      // <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      // <Person 
-      // name={this.state.persons[1].name} 
-      // age={this.state.persons[1].age}
-      // click={ () => this.siwtchNameHandler("Sunil")}
-      // changed= {this.nameChangedHandler}>My Hobbie is Playing Football</Person>
-      // <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      // </div>
       <div>
         {
           this.state.persons.map(
@@ -108,94 +81,43 @@ class App extends Component {
         }
       </div>
       );
+
+      style.backgroundColor = 'red';
+      style[":hover"] = {
+        backgroundColor : 'salmon',
+        color: 'black'
+      }
+    }
+
+    let classes =[];
+
+    if(this.state.persons.length <=2){
+      classes.push('red');//classes= ['red']
+    }
+
+    if(this.state.persons.length <=1){
+      classes.push('bold'); //classes= ['red','bold']
     }
 
     return (
-      <div className="App">
-       <h1>Hi, I am a react app</h1>
-       <p>This is Really working!</p>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am a react app</h1>
+          <p className={classes.join(' ')}>This is Really working!</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler} >Toggle Persons</button>
 
-       {/* <button 
-       style={style}
-       onClick={this.siwtchNameHandler.bind(this,"Suni G")} >Switch Name</button> */}
-
-      <button 
-       style={style}
-       onClick={this.togglePersonsHandler} >Toggle Persons</button>
-
-       {/* {
-         this.state.showPersons ? 
-         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={ () => this.siwtchNameHandler("Sunil")}
-          changed= {this.nameChangedHandler}>My Hobbie is Playing Football</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-         </div> : null
-       } */}
-
-       {persons}   
-      </div>
+        {persons}   
+        </div>
+      </StyleRoot>
+  
     );
 
-    //return React.createElement('div', null, 'h1', 'some text here..');
-    // return React.createElement('div', null, React.createElement('h1', null, 'doe\'s this work now?'));
-    //return React.createElement('div', {className : 'App'}, React.createElement('h1', null, 'doe\'s this work now?'));
+
   }
 }
 
-export default App;
+export default Radium(App);
 
 
-
-
-// import React, { useState } from 'react';
-// import './App.css';
-// import Person from './Person/Person'; // Anything with Caps Start as export is Default
-
-// const app = props => {
-
-//   //useState always returns array of 2 elements : 1 is current state 2nd is function allows to update state
-//     const [personsState , setPersonsState] = useState({
-//         persons : [
-//           {name: "Sunil" , age: 26},
-//           {name: "Manoj" , age: 27},
-//           {name: "pj" , age: 28}
-//         ],
-         
-//       });
-
-//     const [otherState , setOtherState] = useState("other state property")
-
-//     console.log(personsState , otherState);
-
-//     const  siwtchNameHandler = () =>{
-//     //console.log('switch was clicked !');
-//     //Dont Do this :  this.state.persons[0].name ='Sunil Gidd';
-//     setPersonsState({
-//       persons : [
-//         {name: "Sunil Gidd" , age: 27},
-//         {name: "Manoj Dhamone" , age: 28},
-//         {name: "pj" , age: 29}
-//       ]
-//     });
-
-//   };
-
-//     return (
-//       <div className="App">
-//        <h1>Hi, I am a react app</h1>
-//        <p>This is Really working!</p>
-//        <button onClick={siwtchNameHandler} >Switch Name</button>
-//        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-//        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbie is Playing Football</Person>
-//        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-//       </div>
-//     );
-
-
-// }
-
-// export default app;
