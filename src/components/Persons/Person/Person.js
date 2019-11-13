36c22bb4da69893bde5ac45ab24b680399b37969
sunil <sunilgidd051@gmail.com>
2019-11-14 00:04:54 +0530

    component update lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import  './Person.css';
 import Radium from 'radium';
 
-const person = (props) =>{
-
-    const style= {
-        '@media (min-width : 500px )':{
-            width : '450px'
+class Person extends Component{
+    render(){
+        const style= {
+            '@media (min-width : 500px )':{
+                width : '450px'
+            }
         }
+    
+        console.log("[Person.js] Rendering....")
+        return (
+            <div className="Person" style={style}>
+                <h2  onClick= {this.props.click}>I am {this.props.name} and i am {this.props.age} years old</h2>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value ={this.props.name} />
+            </div>
+        );
     }
-
-    console.log("[Person.js] Rendering...")
-    return (
-        <div className="Person" style={style}>
-            <h2  onClick= {props.click}>I am {props.name} and i am {props.age} years old</h2>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value ={props.name} />
-        </div>
-    );
+   
 };
 
-export default Radium(person);
+export default Radium(Person);
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,24 +1,51 @@
-// const persons = (props) => {
-//     return();
-// } 
-
-//as we only returning someting without any additional logic we can shorten as below
-
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person'
 
-const persons = (props) => {
-        console.log("[Persons.js] Rendering...");
-        return props.persons.map(
-        (person, index) => {
-          return <Person 
-          name={person.name} 
-          age={person.age} 
-          click={() => props.clicked(index)}
-          key ={person.id}
-          changed = {  (event) => props.changed(event, person.id)} />
-        }
-      );
+class Persons extends Component {
 
-      }
-export default persons;
+        // static getDerivedStateFromProps(props,state){
+        //   console.log("[Persons.js] getDerivedStateFromProps(props,state)");
+        //   return state;
+        // }
+
+        // componentWillReceiveProps(props){
+        //   console.log("[Persons.js]  componenetWillReceiveProps");
+        // }
+
+        shouldComponentUpdate(nextProps, nextState){
+          console.log("[Persons.js] shouldComponentUpdate(nextProps, nextState)");
+          return true;
+        }
+
+
+        render(){
+          console.log("[Persons.js] Rendering...");
+          return this.props.persons.map(
+          (person, index) => {
+            return <Person 
+            name={person.name} 
+            age={person.age} 
+            click={() => this.props.clicked(index)}
+            key ={person.id}
+            changed = {  (event) => this.props.changed(event, person.id)} />
+          }
+          );
+        } 
+
+        getSnapshotBeforeUpdate(prevProps, prevState){
+          console.log("[Persons.js] getSnapshotBeforeUpdate(prevProps, prevState)");
+          return {message : 'snapshot'};
+        }
+
+        // componentWillUpdate(){
+        //   console.log('[Persons.js] componentWillUpdate');
+        // }
+
+        /* componentWillReceiveProps and componentWillUpdate are not recomnded to use,
+         as they have been renamed or will be removed in future */
+        componentDidUpdate(prevProps, prevState, snapshot){
+          console.log("[Persions.js] componentDidUpdate()");
+          console.log(snapshot);
+        }
+}
+export default Persons;
